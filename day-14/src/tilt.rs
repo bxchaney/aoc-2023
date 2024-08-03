8e038e4f555f6836a72da875d1d4a1cd8e036865
bxchaney <bxchaney@gmail.com>
2024-08-03 14:27:23 -0400

    finished day 14

--- day-14/src/tilt.rs
+++ day-14/src/tilt.rs
@@ -4,77 +4,232 @@ use std::{
     path::Path,
 };
 
-struct Cube {
-    index: i32,
-    stones: i32,
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum Debris {
+    Stone,
+    Cube,
+    Blank,
 }
 
-struct Column {
-    contents: Vec<Cube>,
-    length: usize,
+enum ShiftType {
+    Up,
+    Down,
+    Left,
+    Right,
 }
 
-impl Column {
-    fn from(chars: &Vec<char>) -> Self {
-        let mut contents = vec![];
-        let mut current_cube = Cube {
-            index: -1,
-            stones: 0,
-        };
-        for (i, c) in chars.iter().enumerate() {
-            if *c == 'O' {
-                current_cube.stones += 1;
-            } else if *c == '#' {
-                contents.push(current_cube);
-                current_cube = Cube {
-                    index: i as i32,
-                    stones: 0,
+struct Panel {
+    panel: Vec<Vec<Debris>>,
+    row_rocks: Vec<Vec<usize>>,
+    col_rocks: Vec<Vec<usize>>,
+}
+
+impl Panel {
+    fn new() -> Self {
+        Self {
+            panel: vec![],
+            row_rocks: vec![],
+            col_rocks: vec![],
+        }
+    }
+
+    fn add_row(&mut self, row: String) {
+        let mut new_row = vec![];
+        self.row_rocks.push(vec![]);
+        for (i, c) in row.chars().enumerate() {
+            if self.col_rocks.len() <= i {
+                self.col_rocks.push(vec![]);
+            }
+            let debris = match c {
+                '#' => Debris::Cube,
+                'O' => Debris::Stone,
+                _ => Debris::Blank,
+            };
+            if debris == Debris::Cube {
+                self.col_rocks[i].push(self.panel.len());
+                self.row_rocks[self.panel.len()].push(i);
+            }
+            new_row.push(debris);
+        }
+        self.panel.push(new_row);
+    }
+
+    fn shift(&mut self, shift_type: ShiftType) {
+        match shift_type {
+            ShiftType::Up => {
+                self.vertical_shift(true);
+            }
+            ShiftType::Down => {
+                self.vertical_shift(false);
+            }
+            ShiftType::Left => {
+                self.horizontal_shift(true);
+            }
+            ShiftType::Right => {
+                self.horizontal_shift(false);
+            }
+        }
+    }
+
+    fn vertical_shift(&mut self, north: bool) {
+        let rocks = self.col_rocks.clone();
+        for (i, col_rocks) in rocks.iter().enumerate() {
+            let mut ranges = vec![];
+            if col_rocks.len() == 0 {
+                ranges.push((0, self.panel.len()));
+            } else {
+                let mut start = 0;
+                for j in col_rocks {
+                    ranges.push((start, *j));
+                    start = *j + 1;
+                }
+                ranges.push((start, self.panel.len()));
+            }
+
+            self.sort_vert_range(i, &ranges, north);
+        }
+    }
+
+    fn sort_vert_range(&mut self, column: usize, ranges: &Vec<(usize, usize)>, north: bool) {
+        for (start, stop) in ranges {
+            let mut stones = 0;
+            let mut blanks = 0;
+            for i in *start..*stop {
+                match self.panel[i][column] {
+                    Debris::Stone => {
+                        stones += 1;
+                    }
+                    Debris::Blank => {
+                        blanks += 1;
+                    }
+                    _ => {}
+                }
+            }
+            let primary;
+            let secondary;
+            let mut primary_count;
+            if north {
+                primary_count = stones;
+                primary = Debris::Stone;
+                secondary = Debris::Blank;
+            } else {
+                primary_count = blanks;
+                primary = Debris::Blank;
+                secondary = Debris::Stone;
+            }
+            for i in *start..*stop {
+                if primary_count > 0 {
+                    self.panel[i][column] = primary;
+                    primary_count -= 1;
+                } else {
+                    self.panel[i][column] = secondary;
                 }
             }
         }
-        contents.push(current_cube);
+    }
 
-        Self {
-            contents,
-            length: chars.len(),
+    fn horizontal_shift(&mut self, west: bool) {
+        let rocks = self.row_rocks.clone();
+        for (i, row_rocks) in rocks.iter().enumerate() {
+            let mut ranges = vec![];
+            if row_rocks.len() == 0 {
+                ranges.push((0, self.panel[0].len()));
+            } else {
+                let mut start = 0;
+                for j in row_rocks {
+                    ranges.push((start, *j));
+                    start = *j + 1;
+                }
+                ranges.push((start, self.panel[0].len()));
+            }
+            self.sort_horz_range(i, &ranges, west);
         }
     }
 
-    fn calc_load(&self) -> i32 {
-        self.contents
-            .iter()
-            .map(|x| {
-                Self::pyramid_sum(self.length as i32 - x.index - 1)
-                    - Self::pyramid_sum(self.length as i32 - x.index - 1 - x.stones)
-            })
-            .fold(0, |acc, x| acc + x)
+    fn sort_horz_range(&mut self, row: usize, ranges: &Vec<(usize, usize)>, west: bool) {
+        for (start, stop) in ranges {
+            let mut stones = 0;
+            let mut blanks = 0;
+            for j in *start..*stop {
+                match self.panel[row][j] {
+                    Debris::Stone => {
+                        stones += 1;
+                    }
+                    Debris::Blank => {
+                        blanks += 1;
+                    }
+                    _ => {}
+                }
+            }
+            let primary;
+            let secondary;
+            let mut primary_count;
+            if west {
+                primary_count = stones;
+                primary = Debris::Stone;
+                secondary = Debris::Blank;
+            } else {
+                primary_count = blanks;
+                primary = Debris::Blank;
+                secondary = Debris::Stone;
+            }
+            for j in *start..*stop {
+                if primary_count > 0 {
+                    self.panel[row][j] = primary;
+                    primary_count -= 1;
+                } else {
+                    self.panel[row][j] = secondary;
+                }
+            }
+        }
     }
 
-    fn pyramid_sum(i: i32) -> i32 {
-        (i * (i + 1)) / 2
+    fn get_load(&self) -> i32 {
+        let mut total = 0;
+        let total_rows = self.panel.len() as i32;
+        for (i, row) in self.panel.iter().enumerate() {
+            for d in row {
+                match d {
+                    Debris::Stone => {
+                        total += total_rows - (i as i32);
+                    }
+                    _ => {}
+                }
+            }
+        }
+        return total;
+    }
+
+    pub fn cycle(&mut self) {
+        self.shift(ShiftType::Up);
+        self.shift(ShiftType::Left);
+        self.shift(ShiftType::Down);
+        self.shift(ShiftType::Right);
     }
 }
 
-pub fn tilt(path: &Path) -> (i32, i64) {
+pub fn tilt(path: &Path) -> (i32, i32) {
     let file = File::open(path).expect("Error opening file!");
     let reader = BufReader::new(file);
 
-    let mut panel: Vec<Vec<char>> = vec![];
+    let mut panel = Panel::new();
 
     for l in reader.lines().flatten() {
-        for (i, c) in l.chars().enumerate() {
-            if panel.len() <= i {
-                let new_col = vec![];
-                panel.push(new_col);
-            }
-            panel[i].push(c);
-        }
+        panel.add_row(l);
     }
 
-    let pt1 = panel
-        .iter()
-        .map(|x| Column::from(x).calc_load())
-        .fold(0, |acc, x| acc + x);
+    panel.shift(ShiftType::Up);
+    let pt1 = panel.get_load();
 
-    return (pt1, 0);
+    panel.shift(ShiftType::Left);
+    panel.shift(ShiftType::Down);
+    panel.shift(ShiftType::Right);
+
+    for _ in 0..999 {
+        panel.cycle();
+    }
+
+    let pt2 = panel.get_load();
+
+    return (pt1, pt2);
 }
